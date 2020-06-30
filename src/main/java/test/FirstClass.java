583c1f9bdd906bab1c3cfeb8f8caf290f6f466bb
saoussy.diallo <sanoussy.mamadou.diallo.etu@univ-lille.fr>
2020-06-30 13:18:57 +0200

    for test

---
+++ src/main/java/test/FirstClass.java
@@ -0,0 +1,16 @@
+package test;
+
+public class FirstClass {
+
+	public FirstClass() {
+	}
+	
+	public int firstMethod() {
+		return 1;
+	}
+	public void secondMethod() {
+		int x = 2;
+		//do somthing
+	}
+
+}
